d9928f397f0e45b09ddc35340c2687d9e5b42440
dbp5 <berdie97>
2019-01-09 12:43:42 +0100

    Implemented the counting methods.

--- src/kiosk/VoteCounter.java
+++ src/kiosk/VoteCounter.java
@@ -3,34 +3,47 @@ package kiosk;
 import data.Party;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
  * A class that represents the result in an election site
  */
 public class VoteCounter {
 
-    // TODO Add required data and other methods
+    private int nullVotes = 0;
+    private int blankVotes = 0;
+    private HashMap<String,Integer> partyVotes;
 
-    public VoteCounter(List<Party> validParties) {
-        throw new NotImplementedException();
+    public VoteCounter(Set<Party> validParties) {
+
+        // Initialize the map with the parties, all starting from 0
+        partyVotes = new HashMap<>();
+        for(Party party: validParties)
+        {
+            partyVotes.put(party.getName(), 0);
+        }
     }
 
     public void countParty(Party party) {
-        throw new NotImplementedException();
+        partyVotes.put(
+                party.getName(),
+                partyVotes.get(party.getName()) + 1);
     }
 
     public void countNull() {
-        throw new NotImplementedException();
+        nullVotes+=1;
     }
 
     public void countBlank() {
-        throw new NotImplementedException();
+        blankVotes+=1;
     }
 
     public void scrutinize(Party party) {
-        throw new NotImplementedException();
+
     }
+
+    // region Getters
 
     public int getVotesFor(Party party) {
         throw new NotImplementedException();
@@ -48,4 +61,5 @@ public class VoteCounter {
         throw new NotImplementedException();
     }
 
+    // endregion
 }
