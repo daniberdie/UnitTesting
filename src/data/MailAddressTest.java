8e554dcdec88933b7681e000559610b9d7a0c467
dbp5 <daniberdie>
2019-01-12 18:14:17 +0100

    Created and passed tests for MailAddress.

---
+++ src/data/MailAddressTest.java
@@ -0,0 +1,42 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MailAddressTest {
+
+    @Test
+    public void testConstructorThrowsExceptionWithNullString() {
+
+        Executable exception = () -> new MailAddress(null);
+
+        assertThrows(NullPointerException.class, exception);
+    }
+
+    @Test
+    public void testConstructorThrowsExceptionWithNoAtSymbol() {
+
+        Executable exception = () -> new MailAddress("test.mail.de");
+
+        assertThrows(IllegalArgumentException.class, exception);
+    }
+
+    @Test
+    public void testConstructorThrowsExceptionWithNoDomain() {
+
+        Executable exception = () -> new MailAddress("test@mail");
+
+        assertThrows(IllegalArgumentException.class, exception);
+    }
+
+    @Test
+    public void testConstructorCreatesCorrectInstance() {
+
+        MailAddress testMail = new MailAddress("test@gmail.de");
+
+        assertEquals(MailAddress.class, testMail.getClass());
+    }
+}
